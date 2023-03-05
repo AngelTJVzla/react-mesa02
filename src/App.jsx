7194fd871b02551ea0eb13d57bbf58af4590e4d1
Angel A Ontiveros P <89170175+AngelTJVzla@users.noreply.github.com>
2023-03-05 15:47:21 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//const App = ( ) =><h1>Meu primeiro componente</h1>;
+// const App = () => {
+//     return <h1>Meu primeiro componente</h1>;
+// };
+
+function App() {
+    
+    return (
+        <>
+          {/*  Fragmento */}
+            <h1>"Ola Mundo"</h1>
+           
+      </>
+    );
+}
+
+export default App
